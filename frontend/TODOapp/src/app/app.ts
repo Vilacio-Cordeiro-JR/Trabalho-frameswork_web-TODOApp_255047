522e96cf567cb1c1ce502142271872ed7318b7db
Vilacio-Cordeiro-JR <cordeirojuniorvilacio@gmail.com>
2026-04-21 23:36:48 -0300

    Update API URL in app.ts

--- frontend/TODOapp/src/app/app.ts
+++ frontend/TODOapp/src/app/app.ts
@@ -16,7 +16,7 @@ export class App implements OnInit {
   apiURL: string;
 
   constructor(private http: HttpClient) {
-    this.apiURL = 'https://vilacio255047.up.railway.app';
+    this.apiURL = 'apitarefasvilacio255047.up.railway.app';
     this.READ_tarefas();
   }
 
